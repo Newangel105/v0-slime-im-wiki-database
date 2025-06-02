e7b76652e1245bca8d558b2a66da263f8825b221
NewAngel105 <sapottigpe@gmail.com>
2025-06-02 17:03:07 +0100

    changes

--- app/characters/[characterId]/page.tsx
+++ app/characters/[characterId]/page.tsx
@@ -103,7 +103,7 @@ export default function CharacterDetailPage({ params }: { params: { characterId:
           </Button>
         </Link>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-8">
           {/* Left Column - Character Card */}
           <div className="lg:col-span-1">
             <Card className="bg-gray-800 border-gray-700">
