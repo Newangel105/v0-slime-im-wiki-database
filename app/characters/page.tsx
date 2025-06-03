7a54cb9ea36fe713ab20ec8809f0ca51a452c83a
NewAngel105 <sapottigpe@gmail.com>
2025-06-03 14:32:39 +0100

    changes

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -162,8 +162,12 @@ export default function CharactersPage() {
       if (selectedStars.length > 0 && !selectedStars.includes(character.stars)) {
         return false
       }
-      if (selectedDMGType.length > 0 && !selectedDMGType.includes(character.dmg_type)) {
-        return false
+      if (
+        selectedDMGType.length > 0 &&
+        !selectedDMGType.includes(character.dmg_type) &&
+        !selectedDMGType.includes(`prot_${character.dmg_type}`)
+      ) {
+        return false;
       }
       if (selectedType.length > 0 && !selectedType.includes(character.type)) {
         return false
