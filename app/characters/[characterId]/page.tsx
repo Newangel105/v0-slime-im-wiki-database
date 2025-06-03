fa5625639911490bc09a7bcfc87b9c18553cd3b0
NewAngel105 <sapottigpe@gmail.com>
2025-06-03 12:25:02 +0100

    changes

--- app/characters/[characterId]/page.tsx
+++ app/characters/[characterId]/page.tsx
@@ -170,7 +170,7 @@ export default function CharacterDetailPage({ params }: { params: { characterId:
               />
             )}
             <img
-              src={`/skills/${isEvolution && baseImage ? baseImage : skill.imageName}`}
+              src={`/chars/${character?.id}/${isEvolution && baseImage ? baseImage : skill.imageName}`}
               alt={skill.attackName}
               className="w-10 h-10 z-10 object-contain relative -left-2"
             />
