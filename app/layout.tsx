1b47ce4caad7fb0ddf758e82c017e0abd3be5037
Angel105 <sapottigpe@gmail.com>
2026-04-02 13:52:58 +0100

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
 import type React from "react"
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { SiteNav } from "@/components/site-nav"
 
 const inter = Inter({ subsets: ["latin"] })
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
 export const metadata: Metadata = {
   title: "SLIME.WIKI - Character Database",
   description: "Character database for SLIME - Isekai Memories",
   generator: 'v0.dev',
-  viewport: 'width=device-width, initial-scale=1'
 }
 
 export default function RootLayout({
